33621da3c8b62ff55c7730b2bb79959aefeabd9b
Andy Aldridge <andy@grange85.co.uk>
2024-11-01 00:03:41 +0000

    update test

--- test_bsky_poster.py
+++ test_bsky_poster.py
@@ -4,4 +4,17 @@ def test_get_hashtags():
     assert get_hashtags("#MyHouseInUmbria #RichardLoncraine #MaggieSmith #ChrisCooper #RonnieBarker") == ['MyHouseInUmbria', 'RichardLoncraine', 'MaggieSmith', 'ChrisCooper', 'RonnieBarker']
     assert get_hashtags(" Some rubbish here with some #hashtags and other #interesting stuff") == ['hashtags', 'interesting']
         
+def test_get_did(mocker):
+    mock_data = {"did":"did:plc:wiyfnlefs2t477yqngjcls4r"}
+
+    mock_response = mocker.MagicMock()
+    mock_response.json.return_value = mock_data
+
+    mocker.patch("requests.get", return_value = mock_response)
+
+    result = get_did("handle.bsky.social")
+
+    assert result == mock_data["did"]
+    #assert type(result) is dict
+    #assert result == "did:plc:wiyfnlefs2t477yqngjcls4r"
 
